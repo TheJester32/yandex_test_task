dd10d8e758fd22d2596544faaafc255023338f4e
TheJester32 <bogdanpopoveducation@gmail.com>
2023-10-05 05:29:18 +0300

    animation and header
    animation and header

---
+++ src/modules/animation.js
@@ -0,0 +1,35 @@
+export function animOnScroll() {
+  let animItems = document.querySelectorAll('._anim-items');
+
+  function animOnScroll(params) {
+    for (let i = 0; i < animItems.length; i++) {
+      let animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('_active');
+      } else {
+        animItem.classList.remove('_active');
+      }
+    }
+  }
+
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+
+  animOnScroll();
+
+  window.addEventListener('scroll', animOnScroll);
+}
